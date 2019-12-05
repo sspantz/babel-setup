120808746b01b10d403a7a0ae5109545c1646983
sspantz <sspantiezheng@gmail.com>
2019-12-05 15:23:36 +0800

    config script: build

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const all = [1, 2, 3, 4, 5];
+const random = () => Math.random();
+export {
+  all,
+  random
+}
---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+import './index.js'
+
+describe('test suit', () => {
+  test('availability test', () => {
+    expect(false).toEqual(false);
+  })
+})
